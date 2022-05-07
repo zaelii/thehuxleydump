1fe341e4e5aa75b248edf98b37da15d586db4ba4
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-05-06 23:57:48 -0300

    Update adotar_amor.py

--- Roteiro 2 - Comando condicional/adotar_amor.py
+++ Roteiro 2 - Comando condicional/adotar_amor.py
@@ -1,16 +1,17 @@
 DIFERENCA_IDADE = 16
+
 idade_adotante = int(input())
 if idade_adotante < 18:
     print("Não pode adotar")
 else:
-    se_e_familia = input()
+    irmao_ascendente = input()
     adocao_conjunta = input()
     adotantes_casados = input()
     idade_adotando = input()
     paisdesconhecidos = input()
     consetimento_dos_pais = input()
     consetimento_do_adotando_12 = input()
-    if se_e_familia == "S":
+    if irmao_ascendente == "S":
         print("Não pode adotar")
     elif idade_adotante < (int(idade_adotando) + 16):
         print("Não pode adotar")
