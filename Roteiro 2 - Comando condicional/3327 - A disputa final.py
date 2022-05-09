c208686fa57d03dec0dc6d02049756715eb212e2
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-05-09 19:18:59 -0300

    Create 3327 - A disputa final.py

---
+++ Roteiro 2 - Comando condicional/3327 - A disputa final.py
@@ -0,0 +1,27 @@
+x = int(input())
+
+
+if x == 0:
+    print ("rosa rosa verde rosa")
+elif x == 1:
+    print ("verde rosa verde rosa")
+elif x == 2:
+    print ("verde verde verde rosa")
+elif x == 3:
+    print ("rosa verde verde rosa")
+elif x == 4:
+    print ("rosa rosa rosa verde")
+elif x == 5:
+    print ("verde rosa rosa verde")
+elif x == 6:
+    print ("verde verde rosa verde")
+elif x == 7:
+    print ("rosa verde rosa verde")
+elif x == 8:
+    print ("verde verde verde verde")
+elif x == 9:
+    print ("rosa rosa rosa rosa")
+elif x == 10:
+    print ("rosa rosa verde verde")
+elif x == 11:
+    print ("verde verde rosa rosa")
