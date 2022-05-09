9cb380be653c26991891d789ade4b4a0939987d8
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-05-09 18:31:13 -0300

    Create 1045 - Eficiência de equipe de desenvolvimento.py

---
+++ Roteiro 1 - Operações matemáticas, entrada e saída de dados./1045 - Eficiência de equipe de desenvolvimento.py
@@ -0,0 +1,8 @@
+quantl = float(input())
+quantf = float(input())
+tameq = float(input())
+numb = float(input())
+
+eficiencia = (quantl / quantf) / tameq - 4.2 * numb
+
+print ("{:.1f}".format(eficiencia))
