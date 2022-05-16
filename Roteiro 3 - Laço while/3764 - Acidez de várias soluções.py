53a60703cd933f75d24e1029b4837969997349fa
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-05-16 08:26:26 -0300

    Update and rename acidez_varias_soluçoes.py to 3764 - Acidez de várias soluções.py

---
+++ Roteiro 3 - Laço while/3764 - Acidez de várias soluções.py
@@ -0,0 +1,18 @@
+#FEITO
+
+valor = float(input())
+
+while valor != -1:
+    if valor > 7:
+        print('BASICA')
+
+    elif valor == 7:
+        print('NEUTRA')
+
+    else:
+        print('ACIDA')
+    
+    valor = float(input())
+    
+    
+   
