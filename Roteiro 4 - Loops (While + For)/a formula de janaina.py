cb0b429bb59624bc951843b105839b3ab153096f
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-05-30 08:24:53 -0300

    Create a formula de janaina.py

---
+++ Roteiro 4 - Loops (While + For)/a formula de janaina.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+
+
+formulajanaina = x**2 - 4*y + 5
+delta = y**2-4*x*5
+print(delta)
+raizdelta =delta**0.5
+x1=(-y+raizdelta)/(2*x)
+x2=(+y+raizdelta)/(2*x)
+
+print(x1,x2)
