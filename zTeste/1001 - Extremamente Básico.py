154dffe98ff50d6ee048f245707a788e341f3575
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-06-09 23:39:14 -0300

    Add files via upload

---
+++ zTeste/1001 - Extremamente Básico.py
@@ -0,0 +1,8 @@
+#1001	Extremamente Básico
+
+a = int(input())
+b = int(input())
+
+soma = a + b
+
+print("X = {}".format(soma))
---
+++ zTeste/1002 - Área do Círculo.py
@@ -0,0 +1,6 @@
+raio = float(input())
+
+pi = 3.14159
+area = (pi * (raio**2))
+
+print("A={:.4f}".format(area))
