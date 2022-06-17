a203ba629f46c86a2c915d13d0ce3cb5c7ab5f8f
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-06-17 19:16:21 -0300

    Update 841 - Acima da média.py

--- zExtras/841 - Acima da média.py
+++ zExtras/841 - Acima da média.py
@@ -1,22 +1,19 @@
+#841 - Acima da média
+
 a = float(input())
 b = float(input())
 c = float(input())
-n_acima_media = 0
 
-if a >= 7 and b >= 7 and c >= 7:
-    print("3")
-elif a >= 7 and b >= 7 and c < 7:
-    print("2")
-elif a >= 7 and c >= 7 and b < 7:
-    print("2")
-elif b >= 7 and c >= 7 and a < 7:
-    print("2")
-elif a >= 7 and b < 7 and a < 7:
-    print("1")
-elif b >= 7 and a < 7 and c < 7:
-    print("1")
+if (a <= 10 and b <= 10 and c <= 10):
+    if ((a >= 7) and (b >= 7) and (c >= 7)):
+        print(3)
+    elif ((a >= 7) and (b >= 7) and (c < 7)):
+        print(2)
+    elif ((a >= 7) and (b < 7) and (c >= 7)):
+        print(2)
+    elif ((a < 7) and (b >= 7) and (c >= 7)):
+            print(2)
+    elif ((a >= 7) or (b >= 7) or (c >= 7)):
+            print(1)
 else:
-    print("1")
-
-#2 errors
-#4 passed
+    print("Nenhuma nota acima da média")
