415f0e8f1e8f7d3937d00a209e5ab487edc329c1
Zaeli <75710725+Kurumin7-gif@users.noreply.github.com>
2022-06-17 18:50:33 -0300

    Add files via upload

---
+++ Roteiro 2 - Comando condicional/1070 - Supermercado otimista.py
@@ -0,0 +1,24 @@
+#1070 - Supermercado otimista
+
+dia_da_semana = str(input())
+preco = float(input())
+opcao = str(input())
+nome = str(input())
+
+if dia_da_semana == "seg" or dia_da_semana == "ter" or dia_da_semana == "qua":
+    if opcao == "ouro":
+        print ("O preco do produto",(nome),"no dia",(dia_da_semana),"eh {:.2f}".format(preco/2))
+    else:
+        print ("O preco do produto",(nome),"no dia",(dia_da_semana),"eh {:.2f}".format(preco*2))
+elif dia_da_semana == "qui" or dia_da_semana == "sex":
+    if 10 <= preco <= 100 and opcao == "ouro" or opcao == "prata":
+        print ("O preco do produto",(nome),"no dia",(dia_da_semana),"eh {:.2f}".format(preco*(1/3)))
+    else:
+        print ("O preco do produto",(nome),"no dia",(dia_da_semana),"eh {:.2f}".format(preco*2))
+elif dia_da_semana == "sab":
+    if opcao == "prata":
+        print ("O preco do produto",(nome),"no dia",(dia_da_semana),"eh {:.2f}".format(preco*3))
+    else:
+        print ("O preco do produto",(nome),"no dia",(dia_da_semana),"eh {:.2f}".format(preco*2))
+else:
+    print ("O preco do produto",(nome),"no dia",(dia_da_semana),"eh {:.2f}".format(preco*2))
